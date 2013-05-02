228c5305f60f668df10750949faf701d826b6667
Derek Chiang <derekchiang93@gmail.com>
2013-05-01 22:32:13 -0400

    Using Kristian's getBlockNum

--- src/PageRank.java
+++ src/PageRank.java
@@ -25,14 +25,42 @@ public class PageRank {
     }
 
     private static int getBlockNum(int nodeNum) {
-        if (nodeNum < 2) {
-            return 0;
-        } else if (nodeNum < 4) {
-            return 1;
-        } else if (nodeNum < 6) {
-            return 2;
-        } else {
-            return 3;
+        if(nodeNum >= 685230) {
+            return 68;
+        }
+
+        int[] blockBounds = {10328, 20373, 30629, 40645, 50462, 60841, 70591, 80118, 90497, 100501,
+                             110567, 120945, 130999, 140574, 150953, 161332, 171154, 181514, 191625, 202004,
+                             212383, 222762, 232593, 242878, 252938, 263149, 273210, 283473, 293255, 303043,
+                             313370, 323522, 333883, 343663, 353645, 363929, 374236, 384554, 394929, 404712,
+                             414617, 424747, 434707, 444489, 454285, 464398, 474196, 484050, 493968, 503752,
+                             514131, 524510, 534709, 545088, 555467, 565846, 576225, 586604, 596585, 606367,
+                             616148, 626448, 636240, 646022, 655804, 665666, 675448, 685230
+                            };
+
+        int prevCounter = 68;
+        int counter = 0;
+        Double diff;
+        while(true) {
+
+            if(nodeNum == blockBounds[counter]) {
+                return counter + 1;
+            } else if (nodeNum < blockBounds[counter]) {
+                diff = new Double(Math.abs((counter-prevCounter)/2));
+                if(diff < 1) {
+                    return counter;
+                }
+                prevCounter = counter;
+                counter = counter/2;
+
+            } else {
+                diff = new Double(Math.abs((counter-prevCounter)/2));
+                if(diff < 1) {
+                    return counter + 1;
+                }
+                prevCounter = counter;
+                counter = counter + diff.intValue(); // intValue casts down
+            }
         }
     }
 
